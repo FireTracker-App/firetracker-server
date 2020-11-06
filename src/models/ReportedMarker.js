9743215fddba8de2302392a4099379e550042128
Techdoodle <chris@techchrism.me>
2020-11-06 13:44:49 -0800

    add model for reported marker

---
+++ src/models/ReportedMarker.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const ReportedMarkerSchema = new mongoose.Schema({
+    longitude: {type: Number, required: true},
+    latitude: {type: Number, required: true},
+    reported: {type: Date, default: Date.now()},
+    reporter: {type: String, required: true}
+});
+
+const ReportedMarker = new mongoose.model('ReportedMarker', ReportedMarkerSchema);
+
+module.exports = {ReportedMarkerSchema, ReportedMarker};
