b4ff769c15dc374c6fa3649b86f0272d9909be30
Techdoodle <chris@techchrism.me>
2020-11-06 11:35:07 -0800

    add logging and body parsing for koa

--- src/web.js
+++ src/web.js
@@ -1,8 +1,13 @@
 const Koa = require('koa');
 const helmet = require('koa-helmet');
+const logger = require('koa-logger');
+const koaBody = require('koa-body');
+
 const app = new Koa();
 
+app.use(logger());
 app.use(helmet());
+app.use(koaBody());
 
 app.use(async ctx =>
 {
