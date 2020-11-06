85d9a8bd97db95d677eb1d511dab85666636b6cf
Techdoodle <chris@techchrism.me>
2020-11-06 11:28:52 -0800

    switch Mongoose to use unified topology (this shouldn't be a significant change because the node app is only started when the database is ready)

--- src/db.js
+++ src/db.js
@@ -11,9 +11,7 @@ const {
 
 const options = {
     useNewUrlParser: true,
-    reconnectTries: Number.MAX_VALUE,
-    reconnectInterval: 500,
-    connectTimeoutMS: 10000,
+    useUnifiedTopology: true
 };
 
 const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
