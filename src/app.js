719c418a3a5dfb0c61b8ac1054af8243fb16df20
Techdoodle <chris@techchrism.me>
2020-11-09 14:13:33 -0800

    clean old markers every 15m

--- src/app.js
+++ src/app.js
@@ -1,2 +1,19 @@
 const db = require('./db');
 const web = require('./web');
+const {ReportedMarker} = require('./models/ReportedMarker');
+
+// Cleanup old reported markers
+async function markerCleanup()
+{
+    // Remove markers over 24h old
+    const data = await ReportedMarker.deleteMany({
+        reported: {
+            $lte: Date.now() - (24 * 60 * 60 * 1000)
+        }
+    });
+    console.log('Got data: ');
+    console.log(data);
+}
+
+// Cleanup every 15 minutes
+setInterval(markerCleanup, 15 * 60 * 1000);
